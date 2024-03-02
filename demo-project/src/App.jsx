aa417653bba9c5aed52681250b96f1fb5f972beb
Kaveenxz <kaveenhansithx@gmail.com>
2024-03-02 11:09:21 +0530

    one

---
+++ demo-project/src/App.jsx
@@ -0,0 +1,19 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+
+  function clickMe(){
+    console.log("Im Clicked")
+  }
+  
+  return (
+   <div>
+
+   </div>
+  )
+}
+
+export default App
