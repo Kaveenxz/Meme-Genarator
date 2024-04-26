d53ef8c3e047aeefca63ebc456ab8d64734e5e0e
Kaveenxz <kaveenhansithx@gmail.com>
2024-04-26 22:08:05 +0530

    get a random ing

--- demo-project/src/App.jsx
+++ demo-project/src/App.jsx
@@ -1,12 +1,14 @@
 import Header from './component/Header'
 import './App.css'
 import Meme from './component/Meme'
+import Test from './Test'
 function App() {
 
   return (
     <div>
       <Header></Header>
       <Meme></Meme>
+      {/* <Test></Test> */}
     </div>
   )
 }
--- demo-project/src/component/Meme.jsx
+++ demo-project/src/component/Meme.jsx
@@ -1,20 +1,41 @@
+import { useState } from 'react'
 import memeData from '../memeData'
 
 function Meme() {
 
+    const [meme, setMeme] = useState({
+      topText:"",
+      bottomText:"",
+      randomImage:"https://i.imgflip.com/30b1gx.jpg"
+    })
+
+    const [memeImgage, setMemeImage] = useState(memeData)
+
     const handleClick = ()=>{
-        const memeArray = memeData.data.memes
+        const memeArray = memeImgage.data.memes
         const randomNumber = Math.floor(Math.random() * memeArray.length)
         const url = memeArray[randomNumber].url
-        console.log(url)
-      }
+
+        setMeme(pre => (
+          {
+            ...pre,
+            randomImage:url
+          }
+        ))
+    }
+
+      const isOut = true
   return (
     <main>
-        <form className="form">
+        <div className="form">
             <input type="text" className='form--inputs' placeholder='top-text'/>
             <input type="text" className='form--inputs' placeholder='bottom-text'/>
             <button className='form--button' onClick={handleClick}>Get a new meme image</button>
-        </form>
+
+        </div>
+
+        <img src={meme.randomImage} alt="" className='meme--image'/>
+        <h1>{isOut ? "yes":"no"}</h1>
     </main>
   ) 
 }
